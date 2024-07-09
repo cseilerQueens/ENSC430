a61b73c98eae4b78b4def2f60b4f7e9a0587e383
Christian Seiler <christian.seiler@queensu.ca>
2024-07-09 10:33:19 -0400

    update

---
+++ mySetup.R
@@ -0,0 +1,8 @@
+# Setup
+setwd("C:/Users/seile/Documents/queensu/teaching/classes/ENSC430/tutorials/ENSC430")
+.libPaths("C:/Users/seile/Documents/queensu/teaching/classes/ENSC430/tutorials/ENSC430/renv")
+install.packages("renv")
+renv::init()
+install.packages("rmarkdown")
+install.packages("bookdown")
+

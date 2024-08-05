37a640b5e2e2f33f593b0b6d5f31750d90ac78e5
Christian Seiler <christian.seiler@queensu.ca>
2024-08-05 16:58:16 -0400

    update

--- mySetup.R
+++ mySetup.R
@@ -1,6 +1,7 @@
 # Setup
 setwd("C:/Users/seile/Documents/queensu/teaching/classes/ENSC430/tutorials/ENSC430")
 .libPaths("C:/Users/seile/Documents/queensu/teaching/classes/ENSC430/tutorials/ENSC430/renv")
+
 install.packages("renv")
 renv::init()
 install.packages("rmarkdown")
@@ -8,4 +9,7 @@ install.packages("bookdown")
 install.packages("magrittr")
 install.packages("stringi")
 install.packages("stringr")
+install.packages("deSolve")
+install.packages("scatterplot3d")
+install.packages("EWSmethods")
 
